5989c8e2aa20a1cab092a0732e66bdc516986590
Cole Strain <colestrain2000@gmail.com>
2022-03-03 15:08:21 -0800

    Change Scott's platformer test to use new input system

--- Assets/Scenes/CAFE STUFF/Cafe Scripts/NewBehaviourScript.cs
+++ Assets/Scenes/CAFE STUFF/Cafe Scripts/NewBehaviourScript.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Controls controls;
     public float MovementSpeed = 2;
+
     private void Start()
     {
-        
+        // If this script doesn't have any controls attached to it, attach a new instance of the controls
+        if (controls == null) controls = new Controls();
+        // Enable controls so the player's inputs do stuff
+        controls.Enable();
+        // Another option just for the Move part to be enabled:
+        // controls.player.Move.Enable();
     }
 
-    
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
+        // Move player by reading the Move control value into a variable
+        // The x value of the 2D vector is horizontal movement (A and D)
+        float movement = controls.Land.Move.ReadValue<Vector2>().x;
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
     }
 }
--- Assets/Scenes/CAFE STUFF/Cafe Scripts/Test.cs
+++ Assets/Scenes/CAFE STUFF/Cafe Scripts/Test.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Test : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class Test : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Keyboard.current[Key.Space].wasPressedThisFrame)
             Destroy(gameObject);
     }
 }
