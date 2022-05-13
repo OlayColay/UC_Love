3ee6adb214574ffa8cf747a064045977c91b4d6d
Cole Strain <colestrain2000@gmail.com>
2022-05-13 15:29:14 -0700

    Time on Map

---
+++ Assets/MapTime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class MapTime : MonoBehaviour
+{
+    public Sprite[] timeSprites;
+    public Transform locationsParent;
+
+    void Awake()
+    {
+        int timeOfDay = Inventory.GetTimeOfDay();
+
+        GetComponent<SpriteRenderer>().sprite = timeSprites[timeOfDay];
+
+        Color locationColor = (timeOfDay == 2) ? new Color(0.75f, 0.75f, 0.75f, 1f) : Color.white;
+
+        for(int i = 0; i < locationsParent.childCount; i++)
+        {
+            locationsParent.GetChild(i).GetComponent<SpriteRenderer>().color = locationColor;
+        }
+    }
+}
