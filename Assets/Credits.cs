9534d3f91028dfa3d8b39f9a8388fb815e64b592
Cole Strain <colestrain2000@gmail.com>
2022-05-22 12:12:05 -0700

    Credits draft

---
+++ Assets/Credits.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Credits : MonoBehaviour
+{
+    public float endPosY = 1000f;
+    public float duration = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<RectTransform>().DOMoveY(endPosY + 250, duration).SetEase(Ease.Linear).OnComplete(() => SceneManager.LoadScene(0));
+    }
+}
