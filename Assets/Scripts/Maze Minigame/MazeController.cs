37c5272cb38d2e07bed7374f443f582d0e91fb36
Cole Strain <colestrain2000@gmail.com>
2023-01-22 20:54:31 -0800

    Basic Maze Minigame

---
+++ Assets/Scripts/Maze Minigame/MazeController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public static bool gameWon = false;
+
+    //player stats
+    public float maxSpeed = 7f; //player's max speed
+    public float smoothTime = 0.2f; //time to reach min/max speed
+
+    public TimerScript timer;
+   
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Vector3 currentVelocityPlayer = Vector3.zero;
+
+    void Awake()
+    {
+        gameOver = false;
+        gameWon = false;
+        
+        playerInput = new PlayerInput();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    void Update()
+    {
+        moveInput = playerInput.Player.Move.ReadValue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = moveInput * maxSpeed;
+        rb.velocity =  Vector3.SmoothDamp(rb.velocity, targetVelocity, ref currentVelocityPlayer, smoothTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("you lose!!!!!!!!!!!!!!!!!!");
+            gameOver = true;
+            gameWon = false;
+            timer.TimerOn = false;
+        }
+        else if ((collision.gameObject.tag == "Goal") && (!gameOver))
+        {
+            Debug.Log("you win!!!!!!!!!!!!!!!!!!!");
+            gameOver = true;
+            gameWon = true;
+            timer.TimerOn = false;
+        }
+    }
+
+    public void TimeUp()
+    {
+        Debug.Log("you lose!!!!!!!!!!!!!!!!!!");
+        gameOver = true;
+        gameWon = false;
+    }
+}
---
+++ Assets/Scripts/Maze Minigame/TimerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TextMeshProUGUI TimerTxt;
+    public MazeController mazeController;
+   
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    void Update()
+    {
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+                mazeController.TimeUp();
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
