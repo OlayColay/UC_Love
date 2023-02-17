6011931a3429dcaa5b634d63a08489975e7fec8b
Cole Strain <colestrain2000@gmail.com>
2023-02-17 01:55:12 -0800

    Text display for menu buttons + fade out on hovering over nothing

--- Assets/Scripts/Map/HighlightSelf.cs
+++ Assets/Scripts/Map/HighlightSelf.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class HighlightSelf : MonoBehaviour, IPointerEnterHandler
+public class HighlightSelf : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject externalCameraTarget;
 
@@ -13,7 +14,7 @@ public class HighlightSelf : MonoBehaviour, IPointerEnterHandler
     void Awake()
     {
         cameraControl = Camera.main.GetComponent<CameraControl>();
-        display = FindObjectOfType<DisplayLocation>();
+        display = FindObjectOfType<DisplayLocation>(true);
     }
 
     // When highlighted with mouse.
@@ -21,5 +22,19 @@ public class HighlightSelf : MonoBehaviour, IPointerEnterHandler
     {
         cameraControl.PanCamera(externalCameraTarget != null ? externalCameraTarget : gameObject);
         display.Display(transform.parent.name);
+
+        display.textTweenSeq.Complete(true);
+        display.imgTweenSeq.Complete(true);
+        display.textTweenSeq.Append(display.textField.DOFade(1f, 0.5f)); 
+        display.imgTweenSeq.Append(display.bgImage.DOFade(1f, 0.5f)); 
+    }
+
+    // When highlighted with mouse.
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        display.textTweenSeq.Complete(true);
+        display.imgTweenSeq.Complete(true);
+        display.textTweenSeq.Append(display.textField.DOFade(0f, 0.5f));        
+        display.imgTweenSeq.Append(display.bgImage.DOFade(0f, 0.5f)); 
     }
 }
